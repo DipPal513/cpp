f15357154d051e6a67071e07192619c1e44dc31d
dippal513 <dip.pal.513@gmail.com>
2025-01-06 21:20:44 +0600

    square patterns

---
+++ patterns/square_patter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // square pattern
+   int  n = 5,i,j;
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++){
+            cout << j << " \n";
+        };
+    };
+};
